f520ac3277d8dd81c959fd3f9f2862b4f206a30b
dhruveshk <sunny.rules1@gmail.com>
2019-03-19 02:37:12 +0530

    Update try.py

--- try.py
+++ try.py
@@ -6,6 +6,6 @@ import PyPDF2 as pd
 
 
 pstr = os.popen('pdf2txt.py /home/gangapr/'+str(sys.argv[1])).read()
-c = pstr.count("the")
+c = len(re.findall("(?<![a-z])the(?![a-z])",pstr,re.IGNORECASE))
 with open("tryout.txt", 'w') as f:
     f.writelines(str(c))
